5258f74b3aeecffb7701b38f52bdcf53df54fdc6
Aldo Monteiro <aldo.fsmonteiro@gmail.com>
2023-06-30 00:58:40 -0300

    Handle zip file input and parse dates

--- parse.py
+++ parse.py
@@ -1,13 +1,13 @@
 import fire
 import re
 import os
-from utils import PARSE_TABLE, getFieldParser
+from utils import PARSE_TABLE, FIELDS_TYPES_MAPPING, getFieldParser
 import numpy as np
 import pandas as pd
 import re
 from tqdm import tqdm
 from joblib import Parallel, delayed
-
+from zipfile import ZipFile
 from pandas.core.frame import DataFrame
 
 def toCsv(inputPath: str, outputPath: str):
@@ -19,13 +19,12 @@ def toCsv(inputPath: str, outputPath: str):
     fileName = inputPath.split('/')[-1].strip()
     outputPath = outputPath.strip()
     if os.path.isdir(outputPath):
-        filenameWithoutExtension = re.sub(r'\.txt$', '', fileName, flags=re.IGNORECASE)
+        filenameWithoutExtension = re.sub(r'\.(txt|zip)$', '', fileName, flags=re.IGNORECASE)
         outputFileName = f'{filenameWithoutExtension}.csv'
         outputPath = os.path.join(outputPath, outputFileName)
     if os.path.isfile(outputPath): raise FileExistsError()
-    
-    with open(inputPath, encoding='ISO-8859-1') as f:
-        lines = f.readlines()
+
+    lines = readFile(inputPath)
 
     parsed = toDataFrame(lines)
     print('DataFrame parsed')
@@ -33,28 +32,32 @@ def toCsv(inputPath: str, outputPath: str):
     print(f'Writing to {outputPath}')
     parsed.to_csv(outputPath)
 
-def toParquet(inputPath: str, outputPath: str):
+def toParquet(inputPath: str, outputPath: str, compression='snappy'):
     """
     Convert dataset from "inputPath" to parquet and save it into "outputPath"
     :param inputPath: path to raw dataset to be parsed
     :param outputPath: target path to save resulting file
+    :param compression: {{'snappy', 'gzip', 'brotli', None}}, default 'snappy'
+        Name of the compression to use. Use None for no compression.
     """
+    print(f'inputPath: {inputPath}')
+    print(f'outputPath: {outputPath}')
+    print(f'compression: {compression}')
     fileName = inputPath.split('/')[-1].strip()
     outputPath = outputPath.strip()
     if os.path.isdir(outputPath):
-        filenameWithoutExtension = re.sub(r'\.txt$', '', fileName, flags=re.IGNORECASE)
+        filenameWithoutExtension = re.sub(r'\.(txt|zip)$', '', fileName, flags=re.IGNORECASE)
         outputFileName = f'{filenameWithoutExtension}.parquet'
         outputPath = os.path.join(outputPath, outputFileName)
     if os.path.isfile(outputPath): raise FileExistsError()
-    
-    with open(inputPath, encoding='ISO-8859-1') as f:
-        lines = f.readlines()
+
+    lines = readFile(inputPath)
 
     parsed = toDataFrame(lines)
     print('DataFrame parsed')
     print('shape:', parsed.shape)
     print(f'Writing to {outputPath}')
-    parsed.to_parquet(outputPath)
+    parsed.to_parquet(outputPath, engine='pyarrow', compression=compression)
 
 def toDataFrame(lines) -> DataFrame:
     print('Creating char matrix...')
@@ -77,8 +80,19 @@ def toDataFrame(lines) -> DataFrame:
     print('Parsing fields...')
     for i, field in tqdm(list(PARSE_TABLE.iterrows())):
         df[field.fieldName] = df[field.fieldName].apply(getFieldParser(field.fieldName, field.type))
+        if field.fieldName in FIELDS_TYPES_MAPPING['intFields']:
+            df[field.fieldName] = df[field.fieldName].astype(pd.Int64Dtype())
 
     return df
+
+def readFile(path):
+    if re.match(r'.+\.zip$', path, flags=re.IGNORECASE):
+        with ZipFile(path) as zipfile:
+            filename = zipfile.filelist[0].filename
+            with zipfile.open(filename, 'r') as f:
+                return [line.decode('ISO-8859-1') for line in f.readlines()]
+    with open(path, encoding='ISO-8859-1') as f:
+        return f.readlines()
 
 if __name__ == "__main__":
     fire.Fire({
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import re
+from datetime import date
 
 PARSE_TABLE = pd.read_csv('./parse-table.csv')
 
@@ -20,8 +21,18 @@ def parseFloat(value: str, fieldType: str):
     integerDigitNumber = int(re.findall('\((\d+)\)', fieldType)[0])
     return float(f'{value[:integerDigitNumber]}.{value[integerDigitNumber:]}')
 
+def parseDate(value: str):
+    value = value.strip()
+    if len(value) == 8:
+        year = int(value[:4])
+        month = int(value[4:6])
+        day = int(value[6:])
+        return date(year, month, day)
+
 def getFieldParser(fieldName: str, fieldType: str):
-    if fieldName in FIELDS_TYPES_MAPPING['intFields']:
+    if fieldName in FIELDS_TYPES_MAPPING['dateFields']:
+        return parseDate
+    elif fieldName in FIELDS_TYPES_MAPPING['intFields']:
         return parseInt
     elif fieldName in FIELDS_TYPES_MAPPING['floatFields']:
         return lambda value: parseFloat(value, fieldType)
